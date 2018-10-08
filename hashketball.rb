73bfb6edb9d5754c30aad6f3fce1c7671c929359
Alexander Carlin <ahcarlin@gmail.com>
2018-10-08 04:02:16 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,3 +145,12 @@ def shoe_size(player)
     end
   end
 end
+
+def team_colors(team)
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team 
+      return team_data[:colors]
+    end
+  end
+end
+    
